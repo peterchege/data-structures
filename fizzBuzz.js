50cf95b69a1237bed45366a53263026c43d4e7d2
peter chege <peterchege442@gmail.com>
2020-06-20 17:08:33 +0300

    complete solution

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,9 +1,13 @@
 function fizzBuzz(num) {
     for (let i = 1; i <= num; i++) {
-        if (i % 3 === 0) {
+        if (i % 15 === 0) {
+            console.log('Fizzbuzz')
+        } else if (i % 3 === 0) {
             console.log('fizz')
         } else if (i % 5 === 0) {
             console.log('buzz')
+        } else if (i % 3 === 0 && i % 5 === 0) {
+            console.log('Fizzbuzz')
         } else {
             console.log(i)
         }
