85d828a243e6f3279fc73f465ec1d7034a4a8930
peter chege <peterchege442@gmail.com>
2020-06-21 00:23:58 +0300

    a palindrome algorithm

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+function isPalindrome(string) {
+    string = string.toLowerCase();
+    var charactArr = string.split('');
+    var validateChar = ('abcdefghijklmnopqrstuvwxyz').split('')
+
+    var letterChar = []
+    charactArr.forEach(char => {
+        if (validateChar.indexOf(char) > -1)
+            letterChar.push(char)
+    });
+
+    if (letterChar.join('') === letterChar.reverse().join(''))
+        return true;
+    else return false;
+}
+isPalindrome("Madam I'm Adam");
