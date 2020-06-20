3f3208ea94d731febc6b113c94d815ddc054be8b
peter chege <peterchege442@gmail.com>
2020-06-20 22:25:56 +0300

    clean up

--- harmlessRansomNote.js
+++ harmlessRansomNote.js
@@ -1,7 +1,7 @@
  function harnlessRansomNote(noteText, magazineText) {
      var noteArr = noteText.split(' ');
      var magazineArr = magazineText.split(' ');
-     var magazineObject = {}
+     var magazineObject = {};
 
      magazineArr.forEach(word => {
          if (!magazineObject[word])
@@ -13,16 +13,13 @@
      noteArr.forEach(word => {
          if (magazineObject[word]) {
              magazineObject[word]--;
-             if (magazineObject[word] < 0) {
+             if (magazineObject[word] < 0)
                  noteIsPossile = false;
-             }
-         } else {
-             noteIsPossile = false;
-         }
-
+         } else noteIsPossile = false;
      });
 
      return noteIsPossile;
  }
 
- harnlessRansomNote('', 'this is all the magazine text in the magazine')
+ harnlessRansomNote('this is a secret note from a secret admirer',
+     'this is where all a secret note who is from a secret admirer stays');
