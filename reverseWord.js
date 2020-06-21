99cfe56a4fcacbcf452c01808f9239a572ef6a06
peter chege <peterchege442@gmail.com>
2020-06-21 15:10:42 +0300

    complete code

--- reverseWord.js
+++ reverseWord.js
@@ -1,13 +1,15 @@
-function reverseWord(string) {
-    var wordArr = lowerCaseStr.split(' ');
+function reverseWords(string) {
+    var wordArr = string.split(' ');
     var reversedWordsArr = [];
 
     wordArr.forEach(word => {
         reverseWord = '';
         for (var i = word.length - 1; i >= 0; i--) {
-            reverseWord += word[i]
+            reverseWord += word[i];
         }
         reversedWordsArr.push(reverseWord);
     });
     return reversedWordsArr.join(' ');
 }
+
+reverseWords('this is a string of happiness')
