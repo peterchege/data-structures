dbd45684543ed0408b573bce2bc062e69c727786
peter chege <peterchege442@gmail.com>
2020-06-21 16:37:17 +0300

    completed solution with test value

--- reverseArrayInPlace.js
+++ reverseArrayInPlace.js
@@ -1,7 +1,9 @@
 function reverseArrarInPlace(arr) {
-    for (var i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length / 2; i++) {
         var tempVar = arr[i];
         arr[i] = arr[arr.length - 1 - i];
         arr[arr.length - 1 - i] = tempVar;
     }
+    return arr;
 }
+reverseArrarInPlace([1, 2, 3, 4, 5, 6]);
