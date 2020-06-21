fa169caa43048d401b4b216ade1b8a74738ab5a4
peter chege <peterchege442@gmail.com>
2020-06-21 14:16:26 +0300

    correct code

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,24 +1,26 @@
 function caesarCipher(str, num) {
     num = num % 26;
 
-    lowerCaseStr = str.toLowerCase();
-    alphabet = ('abcdefghijklmnopqrstuvwxz').split('')
-    newString = ''
+    var lowerCaseStr = str.toLowerCase();
+    var alphabet = ('abcdefghijklmnopqrstuvwxz').split('');
+    var newString = '';
 
-    for (var i = 0; i <= lowerCaseStr.lenght; i++) {
+    for (var i = 0; i < lowerCaseStr.length; i++) {
         var currentLetter = lowerCaseStr[i];
         if (currentLetter === ' ') {
             newString += currentLetter;
             continue;
         }
 
-        var currentIndex = alphabet.indexOf(currentLetter)
+        var currentIndex = alphabet.indexOf(currentLetter);
         var newIndex = currentIndex + num;
         if (newIndex > 25) newIndex = newIndex - 26
         if (newIndex < 0) newIndex = 26 + newIndex
         if (str[i] === str[i].toUpperCase()) {
             newString += alphabet[newIndex].toUpperCase();
-        } else newString += alphabet[newIndex]
+        } else newString += alphabet[newIndex];
     }
     return newString;
 }
+
+caesarCipher('Zoo Keeper', 2) // Bqq Mggrgt
