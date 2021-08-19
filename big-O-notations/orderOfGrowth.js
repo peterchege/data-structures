57e0a7579f9bf0887f574ef5f52ac45cfcc84da3
peterchege <peterchege442@gmail.com>
2021-08-19 17:32:12 +0300

    binary search function

--- big-O-notations/orderOfGrowth.js
+++ big-O-notations/orderOfGrowth.js
@@ -43,3 +43,40 @@ function cube (n) {
         }
     }
 }
+
+/* O(log n) = Logarithmic */
+
+/* Recursive function */
+
+function logFunc(n) {
+    if( n === 0 ) return "Done";
+    n = Math.floor(n /2);
+    return logFunc(n);
+    
+}
+
+/* iterative/non-recursive function */
+
+function logn(n) {
+    while (n > 1){
+        n = Math.floor(n /2);
+    }
+}
+
+/* Binary Search function */
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let start = 0;
+let end = arr.length - 1;
+let target = 8;
+
+function binarySearch(arr, start, end, target) {
+    if (start > end) return false;
+
+    midIndex = Math.floor((start + end) / 2);
+
+    if (arr[midIndex] === target) return true;
+    if (arr[midIndex] > target) return binarySearch(arr, start, midIndex -1, target);
+    else if (arr[midIndex] < target) return binarySearch(arr, midIndex + 1, end, target)
+
+}
