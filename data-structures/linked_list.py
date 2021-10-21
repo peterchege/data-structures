9aae00a461acc98724d5253ea5a5f42bae8dff87
peterchege <peterchege442@gmail.com>
2021-10-21 23:52:13 +0300

    adding a new head to the node list

---
+++ data-structures/linked_list.py
@@ -0,0 +1,67 @@
+class node:
+    """
+    An object for storing a single node of a linked list.
+    Models two attributes - data and the link to the next node in the list
+    """
+    data = None
+    next_node = None
+
+    def __init__(self,data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data 
+
+class LinkedList:
+    """
+    Single linked list
+    """
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        """
+        Return the numbers of nodes in the List
+        Takes 0(n) time
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
+
+    def add(self, data):
+        """
+        Adds new Node containing data at the head of the list
+        takes 0(1) time
+        """
+
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+    
+    def __repr__(self):
+        """
+        Return a string representation of the List
+        Takes 0(n) time
+        """
+
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+            
+            current  = current.next_node
+            return '-> '.join(nodes)
