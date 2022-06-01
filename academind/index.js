a0cfa3cf9f4e7d88ec560df587dc872c4eb329ae
peterchege <peterchege442@gmail.com>
2022-06-01 17:12:14 +0300

    finding the sum of an array using linear time complexity

---
+++ academind/index.js
@@ -0,0 +1,40 @@
+// writing a function that retuns the sum of a numbers
+
+// linear Time O(n);
+
+function sumUp(n) { 
+    let result = 0;
+    for (let i = 0; i <= n; i++) {
+        result += i;
+    }
+    return result;
+
+}
+
+// console.log(sumUp(3));
+
+
+// constant time O(1); using a math equation
+
+function sumUpMath(n) { 
+    return (n / 2) * (n + 1);
+}
+
+// console.log(sumUpMath(3));
+
+
+// write an algorith that takes an array of numbers as input and calculates the sum of those numbers
+
+
+function sumOfArray(z) { 
+    let sum = 0;
+    for (let i = 0; i <= z.length - 1; i++) {
+        sum += z[i];
+    }
+    return sum;
+}
+
+f
+
+console.log(sumOfArray([3,5,6,7,8,9,10]));
+
