696276b2978810ae19978b859a5678a2fb381b25
peterchege <peterchege442@gmail.com>
2022-06-09 19:53:32 +0300

    other methods for finding the minimum value

--- academind/quiz1.js
+++ academind/quiz1.js
@@ -1,10 +1,35 @@
 // write an algorithm that should take an array if numbers as input and return the minimum value in the array
 
+//method one
+
 function smallestValue(myArrays) { 
     myArrays.sort(function (a, b) { return a - b });
     return myArrays[0];
 }
 
+//method two
+
+function getMin(myArrays) { 
+    let min = myArrays[0];
+    for (let i = 0; i < myArrays.length; i++) { 
+        if (myArrays[i] < min) {
+            min = myArrays[i];
+        }
+    }
+    return min;
+}
+
+//method three
+
+function findMin(myArrays) { 
+    let min = myArrays[0];
+    for (const num of myArrays) { 
+        if (num < min) {
+            min = num; 
+        }
+    }
+    return min;
+}
 
 // Write an algorith that should take a number as input and return "true" its an even number, "false" if its an odd number
 
@@ -17,5 +42,7 @@ function isEven(numbers) {
     }
 }
 
-console.log(smallestValue([20,4, 7, 2, 6, 10]));
+console.log(smallestValue([20, 4, 7, 2, 6, 10]));
+console.log(getMin([20, 4, 7, 2, 6, 10]));
+console.log(findMin([20,4, 7, 2, 6, 10]));
 console.log(isEven(5));
