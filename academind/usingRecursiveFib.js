e5807fb9a12ba9ff7a602535abecd03d9ac94719
peterchege <peterchege442@gmail.com>
2022-06-27 23:12:25 +0300

    exponential time complexity

---
+++ academind/usingRecursiveFib.js
@@ -0,0 +1,13 @@
+// Return the nth element of the FIbonacci Sequence using recursive
+
+function fib(n) {
+    // [1,1,2,3,5,8]
+    if(n === 0 || n === 1) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+// time complexity: O(2^n)
+
+console.log(fib(15));
