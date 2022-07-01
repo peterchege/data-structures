4da5f50ae3a258d5de75bb35a3a32b3f8f467e93
peterchege <peterchege442@gmail.com>
2022-07-01 15:30:21 +0300

    optimized recursive fun using dynamic prog

---
+++ academind/usingRecursiveFibOptimize.js
@@ -0,0 +1,18 @@
+// Return the nth element of the FIbonacci Sequence using recursive
+
+function fib(n, mem) {
+    // [1,1,2,3,5,8]
+    let res;
+    if(mem[n]) return mem[n];
+    if (n == 0 || n == 1) {
+        res = 1;
+    } else { 
+        res = fib(n - 1, mem) + fib(n - 2, mem);
+    }
+    mem[n] = res;
+    return res;
+}
+
+// time complexity: O(n)
+
+console.log(fib(15));
