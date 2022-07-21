1a3c95782a9f4ed93baff0893626b328628b745e
peterchege <peterchege442@gmail.com>
2022-07-22 00:26:38 +0300

    check

---
+++ academind/test1.js
@@ -0,0 +1,8 @@
+const languages = [
+    { id: 3, name: 'Basic' },
+    { id: 1, name: 'Python' },
+    { id: 2, name: 'JavaScript' },
+    { id: 4, name: 'C#' }, 
+];
+
+console.log(languages.map((lan) => lan.name)[1]);
