b4da24f6ed018f4fa66142d59f666f0436aebc49
peterchege <peterchege442@gmail.com>
2022-07-24 20:02:20 +0300

    using recursive function for binary search

--- academind/search-algorithms/binarySearch.js
+++ academind/search-algorithms/binarySearch.js
@@ -22,10 +22,41 @@ function findElement(sortedArr, element) {
 
     }
 
-
 }
 
 
 arr = [1, 5, 9, 13, 99, 100];
 
 console.log(findElement(arr, 99));
+
+
+// Usig recursive function to find the index of the element
+
+
+function findElement2(sortedArr, element, offset) {
+    
+    let startIndex = 0;
+    let endIndex = sortedArr.length - 1;
+
+    let midvalue = Math.floor((endIndex - startIndex) / 2);
+    let midElement = sortedArr[midvalue];
+
+    if (midElement === element) {
+         return midvalue + offset;
+    } 
+
+    if (midElement < element) {
+        startIndex = midvalue + 1;
+        offset = offset + midvalue + 1;
+
+    } else { 
+        endIndex = midvalue - 1;
+    }
+    return findElement2(sortedArr.slice(startIndex, endIndex+1), element, offset);
+
+}
+
+
+arr2 = [1, 5, 9, 13, 99, 100];
+
+console.log(findElement2(arr2, 99, 0));
