ece0dea630df14394a8d952afc16d3d37fa95376
peterchege <peterchege442@gmail.com>
2022-07-24 18:26:48 +0300

    linear search algorithms

---
+++ academind/search-algorithms/linearSearch.js
@@ -0,0 +1,36 @@
+// linear search algorithm is a search algorithm that goes through all the elements of an array and checks if the element is equal to the searched element.
+
+function findElement(arr, element) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === element) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+const arr = [5, 3, 10, -10, 33, 51];
+
+
+console.log(findElement(arr, 10));
+
+
+
+
+//Using map of object function to find the index of the element
+
+function findElement2(arr2, element) {
+    let index = 0;
+    for (const item of arr2) {
+        if (item == element) {
+            return index;
+        }
+        index++;
+    }
+}
+
+
+const arr2 = [20, -30, 10, 50, -60, 70];
+
+
+console.log(findElement2(arr2, 70));
