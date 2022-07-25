959ea18b5d3a96340ff89d90f33d8008ce2f0740
peterchege <peterchege442@gmail.com>
2022-07-25 11:50:29 +0300

    mergesort algorithm function

---
+++ academind/sort-algorithms/mergeSort.js
@@ -0,0 +1,43 @@
+function sort(arr) { 
+    if (arr.length < 2) {
+        return arr;
+    }
+
+    if (arr.length === 2) { 
+        return arr[0] > arr[1] ? [arr[1], arr[0]] : arr;
+    }
+
+    const middleIndex = Math.floor(arr.length / 2);
+    const leftArr = arr.slice(0, middleIndex);
+    const rightArr = arr.slice(middleIndex);
+    
+    console.log('LeftArray:', leftArr);
+    console.log('RigthArray:', rightArr);
+    
+    const leftSortedArray = sort(leftArr);
+    const rightSortedArray = sort(rightArr);
+
+    console.log('LeftSortedArray:', leftSortedArray);
+    console.log('RigthSortedArray:', rightSortedArray);
+    
+    const mergeArr = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < leftSortedArray.length || rightIndex < rightSortedArray.length) {
+
+        if (leftIndex >= leftSortedArray.length || leftSortedArray[leftIndex] > rightSortedArray[rightIndex]) {
+            mergeArr.push(rightSortedArray[rightIndex])
+            rightIndex++;
+        } else { 
+            mergeArr.push(leftSortedArray[leftIndex]);
+            leftIndex++;
+        }
+    }
+    
+    return mergeArr;
+
+}
+
+const sortedArray = sort([-10, 33, 5, 24, 10, 9, 3, -19, -99, 100]);
+console.log(sortedArray);
