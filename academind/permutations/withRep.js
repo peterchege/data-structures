00b4a5abb74f6b438b717a2a96009d32ab95e717
peterchege <peterchege442@gmail.com>
2022-08-03 16:10:01 +0300

    using permutattion for repeatitive values

---
+++ academind/permutations/withRep.js
@@ -0,0 +1,23 @@
+// with repetitions
+
+function getPermutations(options, length) {
+  const permutations = [];
+
+  if (length === 1) {
+    return options.map((option) => [option]);
+  }
+
+  const partialPermutations = getPermutations(options, length - 1);
+  for (const option of options) {
+    for (const partialPermutation of partialPermutations) {
+      permutations.push([option].concat(partialPermutation));
+    }
+  }
+
+  return permutations;
+}
+
+const digits = [1, 2, 3];
+const resultLength = 3;
+
+console.log(getPermutations(digits, resultLength));
